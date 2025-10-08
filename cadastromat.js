eb0429d9f6e0b4af97a76cd338861df1320c0f79
João Henrique Medeiros Proença <joao.h.proenca@gmail.com>
2025-10-07 23:30:42 -0300

    organizando scripts

---
+++ cadastromat.js
@@ -0,0 +1,44 @@
+// Script da seção Material
+
+let codigo = [] //Lista para verificar se código já existe
+let resMat = document.getElementById('resMat')
+let btnCadastrar = document.getElementById('btnCadastrar')
+
+//Cadastro de material
+btnCadastrar.addEventListener('click', function(){
+  let cadMat = {
+  nome: document.getElementById('nomeMat').value,
+  codigo: Number(document.getElementById('codMat').value),
+  descricao: document.getElementById('desMat').value,
+  imagem: document.getElementById('imgMat').value
+  }
+
+  //Verifica se o código já existe
+  let pesq = codigo.indexOf(cadMat.codigo)
+
+  if(pesq == -1){
+     codigo.push(cadMat.codigo)
+
+  const addlinha = document.createElement('tr')
+
+  addlinha.innerHTML = `
+    <td>${cadMat.codigo}</td>
+    <td>${cadMat.nome}</td>
+    <td>${cadMat.descricao}</td>
+    <td>IMG</td>
+    `
+  alert('Material cadastrado com sucesso!')
+  resMat.appendChild(addlinha)
+
+  document.getElementById('nomeMat').value = ''
+  document.getElementById('codMat').value = ''
+  document.getElementById('desMat').value = ''
+  document.getElementById('imgMat').value = ''
+  }else{
+   alert('O código digitado já existe!')
+    document.getElementById('codMat').value = ''
+
+  }
+  
+
+})
