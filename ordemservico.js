5a1c0b66fd7126fdda9178047b2f494173485829
João Henrique Medeiros Proença <joao.h.proenca@gmail.com>
2025-10-08 00:29:52 -0300

    update ordem de serviço

---
+++ ordemservico.js
@@ -0,0 +1,41 @@
+let btnCadastrar = document.getElementById('btnCadastrarOS')
+let resOS = document.getElementById('resOS')
+
+btnCadastrar.addEventListener('click', function() {
+    // Pega os valores dos campos
+    let codigo = document.getElementById('codigoOS').value.trim()
+    let descricao = document.getElementById('descricaoOS').value.trim()
+    let status = document.getElementById('statusOS').value.trim()
+
+    // Verifica se algum campo está vazio
+    if (codigo === '' || descricao === '' || status === '') {
+        alert('Por favor, preencha todos os campos antes de cadastrar.')
+        return // interrompe a execução se faltar algo
+    } else {
+     // Se tudo estiver preenchido, cria o objeto
+    let cadOS = {
+        codigo: Number(codigo),
+        descricao: descricao,
+        status: status
+    }
+
+    const addlinha = document.createElement('tr')
+
+    addlinha.innerHTML = `
+        <td>${cadOS.codigo}</td>
+        <td>${cadOS.descricao}</td>
+        <td>${cadOS.status}</td>
+        `
+    alert('Cadastro realizado com sucesso!')
+    resOS.appendChild(addlinha)    
+
+    
+
+    //Limpa os campos preenchidos após cadastro
+    document.getElementById('codigoOS').value =''
+    document.getElementById('descricaoOS').value = ''
+    document.getElementById('statusOS').value = ''
+    }
+
+    
+})
